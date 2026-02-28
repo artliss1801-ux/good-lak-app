dcc4257772b9728e548a11a24ded21bcf33476b2
artliss1801-ux <artliss1801@gmail.com>
2026-02-28 22:14:19 +0300

    Add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { GoodLakApp } from '@/components/good-lak/GoodLakApp';
+
+export default function Home() {
+  // Показываем сразу клиентское приложение
+  // Вход для мастера доступен на экране входа через кнопку "Вход для мастера"
+  return <GoodLakApp />;
+}
